c7977a56f09187f1676caf34471555659b7b9241
Alex Lishinski <alexlishinski@gmail.com>
2021-11-02 15:59:24 -0400

    week 12

--- homework/app.R
+++ homework/app.R
@@ -15,11 +15,13 @@ library(car)
 library(here)
 library(tidyverse)
 
-data <- read_csv(here("content", "data", "undergrad_data.csv"))
+data <- read_csv(here("undergrad_data.csv"))
 data <- data[,-c(1:2)]
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+    
+    theme = bslib::bs_theme(bootswatch = "darkly"),
 
     # Application title
     titlePanel("Regression Modeler"),
@@ -36,66 +38,73 @@ ui <- fluidPage(
                                "independent variables",
                                choices = colnames(data)),
             
+            actionButton("fit",
+                         "Fit the Model")
+            
             
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           textOutput("message"),
-           tableOutput("coefTable"),
-           tableOutput("modelFit"),
-           plotOutput("regPlot")
-        )
-    )
+                    tabsetPanel(
+                        tabPanel(
+                            p("Coefficients"),
+                            tableOutput("coefTable")
+                        ),
+                        tabPanel(
+                            p("Model Summary"),
+                            tableOutput("modelFit")
+                        ),
+                        tabPanel(
+                            p("Fitted vs residuals"),
+                            plotOutput("regPlot", click = "plot_click"),
+                            verbatimTextOutput("click")
+                        )
+                        
+                        
+                    )
+
+                )
+            )
 )
+
 
 # Define server logic to run regression models and produce output
 server <- function(input, output) {
+    
+    rhs <- reactive({paste(input$ivs, collapse = " + ")})
+    
+    formula <- reactive({paste(input$dv, rhs(), sep = " ~ ")})
+    
+    fit <- eventReactive(input$fit, {
+        lm(formula(), data = data)
+    })
 
     output$coefTable <- renderTable({
-        
-        rhs <- paste(input$ivs, collapse = " + ")
-        
-        formula <- paste(input$dv, rhs, sep = " ~ ")
-        
-        fit <- lm(formula, data = data)
 
-        tidy(fit)
+        tidy(fit())
         
-
     })
-    
-        
-    observeEvent(input$ivs, { message("model has been fit")})
-    
-    
-    observe({"The Model Has Been FIt"}) %>% bindEvent(input$ivs)#, {renderText("The Model has been Fit")})
     
     
     output$modelFit <- renderTable({
         
-        rhs <- paste(input$ivs, collapse = " + ")
-        
-        formula <- paste(input$dv, rhs, sep = " ~ ")
-        
-        fit <- lm(formula, data = data)
-        
-        glance(fit)
+        glance(fit())
     })
     
     output$regPlot <- renderPlot({
         
-        rhs <- paste(input$ivs, collapse = " + ")
-        
-        formula <- paste(input$dv, rhs, sep = " ~ ")
-        
-        fit <- lm(formula, data = data)
-        
-        ggplot(model.frame(fit)) + 
-            geom_point(aes(x = fitted(fit), y = residuals(fit)))
+        ggplot(model.frame(fit())) + 
+            geom_point(aes(x = fitted(fit()), y = residuals(fit())))
         
     })
     
+    output$click <- renderPrint({
+        req(input$plot_click)
+        x <- input$plot_click$x
+        y <- input$plot_click$y
+        cat("Fitted: ", x, "Residual: ", y)
+    })
 
 }
 
